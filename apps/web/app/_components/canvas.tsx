3638c5625a3ca2c1f534c8c8608a45806f85232a
SerBakon <serbakonyt@gmail.com>
2026-07-12 17:01:01 -0700

    feat: Camera Controls for wormhole navigation

--- apps/web/app/_components/canvas.tsx
+++ apps/web/app/_components/canvas.tsx
@@ -1,12 +1,19 @@
 "use client";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { CameraControls, Stars } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Wormhole from "./wormhole";
 
 export default function HomeCanvas() {
   return (
     <Canvas camera={{ position: [0, 0, 8], fov: 75 }}>
-      <OrbitControls autoRotate autoRotateSpeed={0.1} />
+      <CameraControls
+        minPolarAngle={-Infinity}
+        maxPolarAngle={Infinity}
+        azimuthRotateSpeed={0.5}
+        polarRotateSpeed={0.5}
+        minDistance={1}
+        maxDistance={20}
+      />
       <Stars radius={200} depth={100} count={5000} factor={4} saturation={0} />
       <color attach="background" args={["#000"]} />
       <Wormhole />
