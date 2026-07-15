bda6cbc878374782902a9785b38f7b95c7206cec
SerBakon <serbakonyt@gmail.com>
2026-07-15 11:53:46 -0700

    feat: starry background + color grading

--- apps/web/app/_components/background.tsx
+++ apps/web/app/_components/background.tsx
@@ -1,9 +1,20 @@
-import { Cloud, Stars } from "@react-three/drei";
+import { Stars, useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 export default function Background() {
+  const texture = useTexture("/8k_milkyway.jpg");
+  texture.colorSpace = THREE.SRGBColorSpace;
+
   return (
     <>
-      <color attach="background" args={["#050510"]} />
+      <mesh>
+        <sphereGeometry args={[500, 64, 64]} />
+        <meshBasicMaterial
+          map={texture}
+          side={THREE.BackSide}
+          color="#cccccc"
+        />
+      </mesh>
       <Stars radius={200} depth={100} count={5000} factor={4} saturation={1} />
     </>
   );
